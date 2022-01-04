10c832d260fc715bc8449c17c96df018c0219fc7
bsdesigncompany <bsdcompany.dev@gmail.com>
2022-01-05 01:10:24 +0300

    Add 070-outputs.tf +  correct terraform.tfvars

---
+++ 070-outputs.tf
@@ -0,0 +1,10 @@
+/*
+output "instances" {
+  value     = openstack_compute_instance_v2.server
+  sensitive = true
+}
+
+output "OS_image_for_install" {
+  value = data.openstack_images_image_v2.disk_image
+}
+*/
